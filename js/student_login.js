5c5e143783e8dce8e0be7f58889215b7501da2f2
Chaitanya Jambhulkar <chaitanyajambhulkar768@gmail.com>
2024-04-12 23:23:41 +0530

    added login db for admin and students

--- js/student_login.js
+++ js/student_login.js
@@ -29,3 +29,24 @@ const texts = [
     const typingElement = document.getElementById('typing-text');
     typeWriter(texts[textIndex], 0, typingElement);
   };
+
+  async function login() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('db/users.json');
+        const userData = await response.json();
+
+        const user = userData.find(user => user.email === email && user.password === password);
+        
+        if (user) {
+            window.location.href = "student_home.html";
+        } else {
+            alert("Invalid email or password. Please try again.");
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+        alert("An error occurred during login. Please try again later.");
+    }
+}
