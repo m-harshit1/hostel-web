844414475ea78068b34efa40605b46327d5f455c
Chaitanya Jambhulkar <chaitanyajambhulkar768@gmail.com>
2024-04-12 22:32:21 +0530

    added complain js

--- js/complain.js
+++ js/complain.js
@@ -1,17 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const composeBox = document.querySelector('.compose_box');
+function toggleComplainBox() {
     const complainBox = document.querySelector('.compose_complain_box');
-    const crossIcon = document.querySelector('.cross');
-
-    function toggleComplainBox() {
-        if (complainBox.style.display === 'none' || complainBox.style.display === '') {
-            complainBox.style.display = 'block';
-        } else {
-            complainBox.style.display = 'none';
-        }
-    }
-
-    composeBox.addEventListener('click', toggleComplainBox);
-
-    crossIcon.addEventListener('click', toggleComplainBox);
-});
+    complainBox.style.display = complainBox.style.display === 'none' ? 'block' : 'none';
+}
