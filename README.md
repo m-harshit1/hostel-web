b0a012eac35ef0b3deebb798030cd6cbea8050d8
Arnav Sharda <asharda7898@gmail.com>
2024-12-02 13:38:52 +0530

    updated and added node modules , tailwind , and made a responsive index.html

--- README.md
+++
@@ -1,3 +0,0 @@
-# Hostel Web Management System
-
-This is a website for college hostels to manage various activities and forms ranging from leave to attendance.
--- js/complain.js
+++
@@ -1,4 +0,0 @@
-function toggleComplainBox() {
-    const complainBox = document.querySelector('.compose_complain_box');
-    complainBox.style.display = complainBox.style.display === 'none' ? 'block' : 'none';
-}
